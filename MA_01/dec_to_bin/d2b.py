c433c06ea83dfe58c9cfa11ea7c5bc79ebb71436
abenf <agbenfante@gmail.com>
2022-01-16 15:28:50 -0500

    Created file 'machine_assignment_01.py'

--- MA_01/dec_to_bin/d2b.py
+++ MA_01/dec_to_bin/d2b.py
@@ -1,5 +1,5 @@
 
-DecBinTuple = (float, float)
+DecBinTuple = (float, float, int) # (decimal, binary, sign)
 MAX_DIGITS = 8
 
 def set_max_digits(value : int):
@@ -7,24 +7,27 @@ def set_max_digits(value : int):
     MAX_DIGITS = value
 
 def results_header(width : int=(MAX_DIGITS + 4)) -> str:
-    return "|{:^{w}}|{:^{w}}|\n".format('Decimal', 'Binary', w = width) + \
-          ("|" + "-"*(width))*2 + '|\n'
+    return f'|{"Decimal":^{width}}|{"Binary":^{width}}\n' + ("|" + "-"*(width))*2 + '|\n'
+          
+def result_string(my_dec : float, my_bin : float, sign : int, width : int=(MAX_DIGITS + 2)) -> str:
+    s : str = ' '
+    if sign < 0:
+        s = '-'
+    if len(str(my_bin)) > width:
+        my_bin = round(my_bin, MAX_DIGITS)
+    return f'|{s}{my_dec:<{width}} |{s}{my_bin:<{width}} |\n'
 
-def result_string(nums : DecBinTuple, precision : int=(MAX_DIGITS + 2)) -> str:
-    return f'| {nums[0]:<{precision}} | {nums[1]:<{precision}} |\n'
-
-def dec_to_bin(my_dec : float, round : bool=False, base : int=2, precision : int=MAX_DIGITS) -> DecBinTuple:
+def dec_to_bin(my_dec : float, base : int=2, precision : int=MAX_DIGITS) -> DecBinTuple:
+    sign : int = (my_dec >= 0) - (my_dec < 0)
     i : int = 0
-    d_i : float = my_dec
+    d_i = my_dec = abs(my_dec)
     c_i : float = 0
     my_bin : float = 0
     while my_dec != 0 and i < precision:
         c_i = base * d_i - 1
         d_i *= base
         if c_i >= 0:
-            my_bin += 1 * 10**-(i+1)
+            my_bin += 10**-(i + 1)
             d_i = c_i
         i += 1
-    if round and my_dec != 0:
-        my_bin += 1 * 10**-(i) # Round up
-    return (my_dec, my_bin)
+    return (my_dec, my_bin, sign)
--- MA_01/main.py
+++ MA_01/main.py
@@ -5,7 +5,7 @@ def main(*args):
     results : str = results_header()
     for arg in args[1:]:
         dec_bin : DecBinTuple = dec_to_bin(float(arg))
-        results += result_string(dec_bin)
+        results += result_string(*dec_bin)
     print(results)
 
 if __name__ == '__main__':
