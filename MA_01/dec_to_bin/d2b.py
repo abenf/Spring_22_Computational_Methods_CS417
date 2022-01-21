537d3d938900628c26cb5a31c3374df8c909f02c
abenf <agbenfante@gmail.com>
2022-01-21 17:02:18 -0500

    Added BinListTuple type alias; in convert_fractional(float, int, int) -> list[int] updated digit_i scan to account for bases other than base-2

--- MA_01/dec_to_bin/d2b.py
+++ MA_01/dec_to_bin/d2b.py
@@ -1,6 +1,7 @@
 from math import modf, log
 
-DecBinTuple = (float, [list[int], list[int]], int) #(decimal, binary -> [fractional, integer], sign)
+BinListTuple = list[list[int], list[int]] #fractional and whole number parts
+DecBinTuple = (float, BinListTuple, int) #(decimal, binary -> [integer, fractional], sign)
 MAX_DIGITS = 8 #precision
 BASE_INT = 2 #number base
 
@@ -18,7 +19,7 @@ def results_header(width : int=(MAX_DIGITS + 4)) -> str:
 def binary_string(bin_list : list[int]) -> str:
     return ''.join(str(zeros_and_ones) for zeros_and_ones in bin_list) #convert contents of list[int] to strings and join
           
-def result_string(my_decimal : float, my_binary : list[list[int], list[int]], sign : int, width : int=(MAX_DIGITS + 2)) -> str:
+def result_string(my_decimal : float, my_binary : BinListTuple, sign : int, width : int=(MAX_DIGITS + 2)) -> str:
     s : str = (sign >= 0)*' ' + (sign < 0)*'-' #add a minus sign if number is negative
     bin_str =  binary_string(my_binary[0]) 
     try: #don't print fractional if my_binary is a whole number
@@ -38,7 +39,7 @@ def cut_trailing_zeros(my_list : list[int]) -> list[int]:
 
 def dec_to_bin(my_decimal : float, base : int=BASE_INT, precision : int=MAX_DIGITS) -> DecBinTuple:
     sign : int = (my_decimal >= 0) - (my_decimal < 0) #determine and store sign of inputted decimal number
-    my_binary_list = []
+    my_binary_list : BinListTuple = []
     fractional, integer = modf(abs(my_decimal)) #split integer and fractional elements
     integer = int(integer)
 
@@ -75,6 +76,7 @@ def convert_fractional(buffer : float, base : int, precision : int) -> list[int]
         digit_i[i] = int(buffer)
         buffer -= digit_i[i]
         i += 1
-    if 1 in digit_i:
-        return cut_trailing_zeros(digit_i)
+    for elem in digit_i:
+        if elem != 0:
+            return cut_trailing_zeros(digit_i)
     return []
