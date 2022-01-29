b26fb1d709b61c9b5cb7348c329e435ee4a8a15c
abenf <agbenfante@gmail.com>
2022-01-28 23:55:15 -0500

    commit

--- MA_02/dec_to_bin/d2b.py
+++ MA_02/dec_to_bin/d2b.py
@@ -1,76 +1,115 @@
 from math import modf, log
 
-BinListTuple = list[list[int], list[int]] #fractional and whole number parts
-DecBinTuple = (float, BinListTuple, int) #(decimal, binary -> [integer, fractional], sign)
-MAX_DIGITS = 8 #precision
-BASE_INT = 2 #number base
+AltBaseListTuple = list[list[int], list[int]] # fractional and whole number parts
+DecAltTuple = (float, AltBaseListTuple, int) # (decimal, alt base number -> [integer, fractional], sign)
+MAX_DIGITS = 8  # precision
+BASE_INT = 2    # number base
+GT_25 = False   # number base is greater than 25 (set in set_base_int)
 
-def set_max_digits(value : int): #for use with future command line flags
+
+def convert_number_list(number_list : list[int]) -> list[str]:
+    for k, digit in enumerate(number_list):
+        number_list[k] = convert_gt_ten_digit(digit)
+    return number_list
+
+def convert_gt_ten_digit(value : int):
+    gt_ten_digit : str = ""
+    if value >= 26:
+        i, value = divmod(value, 26) # add Zs til value < 26
+        gt_ten_digit += i*'Z'
+    if value > 9:
+        gt_ten_digit += chr(55 + value) # add alpha representation of numbers greater than 9
+    else:
+        gt_ten_digit += str(value)
+    gt_ten_digit += ";"*GT_25
+    return gt_ten_digit
+
+def set_max_digits(value : int): 
     global MAX_DIGITS
+    value = int(value)
+    if value < 1 or value > 20:
+        print("Invalid precision; revert to default")
+        return
     MAX_DIGITS = value
 
-def set_base_int(value : int): #for use with future command line flags
+def set_base_int(value : int): 
     global BASE_INT
+    global GT_25
+    value = int(value)
+    if value < 2:
+        print("Invalid base; revert to default")
     BASE_INT = value
+    if value > 25:
+        GT_25 = True
 
 def results_header(width : int=(MAX_DIGITS + 4)) -> str:
-    return f'|{"Decimal":^{width}}|{"Binary":^{width}}|\n' + ("|" + "-"*(width))*2 + '|\n'
+    base_str = "Base " + str(BASE_INT)
+    return f'|{"Decimal":^{width}}|{base_str:^{width}}|\n' + ("|" + "-"*(width))*2 + '|\n'
 
-def binary_string(bin_list : list[int]) -> str:
-    return ''.join(str(zeros_and_ones) for zeros_and_ones in bin_list) #convert contents of list[int] to strings and join
+def alt_base_string(alt_base_list : list[int]) -> str:
+    return ''.join(str(zeros_and_ones) for zeros_and_ones in alt_base_list) # convert contents of list[int] to strings and join
           
-def result_string(my_decimal : float, my_binary : BinListTuple, sign : int, width : int=(MAX_DIGITS + 2)) -> str:
-    s : str = (sign >= 0)*' ' + (sign < 0)*'-' #add a minus sign if number is negative
-    bin_str =  binary_string(my_binary[0]) 
-    try: #don't print fractional if my_binary is a whole number
-        bin_str += '.' + binary_string(my_binary[1])
+def result_string(my_decimal : float, my_alt_base_num : AltBaseListTuple, sign : int, width : int=(MAX_DIGITS + 2)) -> str:
+    s : str = (sign >= 0)*' ' + (sign < 0)*'-' # add a minus sign if number is negative
+    alt_base_str =  alt_base_string(my_alt_base_num[0]) 
+    try: # don't print fractional if my_alt_base_num is a whole number
+        alt_base_str += '.' + alt_base_string(my_alt_base_num[1])
     except IndexError:
         pass
-    return f'|{s}{my_decimal:<{width}} |{s}{bin_str[:width]:<{width}} |\n'
-
+    return f'|{s}{my_decimal:<{width}} |{s}{alt_base_str[:width]:<{width}} |\n'
 
 def cut_trailing_zeros(my_list : list[int]) -> list[int]:
     i : int = 0
     val_at_index_is_zero : bool = True
     while val_at_index_is_zero:
         i -= 1
-        val_at_index_is_zero -= (my_list[i]) #search backwards for final 1
+        val_at_index_is_zero -= (my_list[i] != 0) # search backwards for final non-zero digit
+    if i == -1:
+        return my_list
     return my_list[:i+1]
 
-def dec_to_bin(my_decimal : float, base : int=BASE_INT, precision : int=MAX_DIGITS) -> DecBinTuple:
-    sign : int = (my_decimal >= 0) - (my_decimal < 0) #determine and store sign of inputted decimal number
-    my_binary_list : BinListTuple = []
-    fractional, integer = modf(abs(my_decimal)) #split integer and fractional elements
+def dec_to_alt(my_decimal : float) -> DecAltTuple:
+    sign : int = (my_decimal >= 0) - (my_decimal < 0) # determine and store sign of inputted decimal number
+    my_alt_base_list : AltBaseListTuple = []
+    fractional, integer = modf(abs(my_decimal)) # split integer and fractional elements
     integer = int(integer)
 
-    my_binary_list.append(convert_integer(integer, base))
-    bin_fractional = convert_fractional(fractional, base, precision)
-    if bin_fractional:
-        my_binary_list.append(bin_fractional)
+    my_alt_base_list.append(convert_integer(integer))
+    alt_fractional = convert_fractional(fractional)
+    if alt_fractional:
+        my_alt_base_list.append(alt_fractional)
 
-    return (abs(my_decimal), my_binary_list, sign)
+    return (abs(my_decimal), my_alt_base_list, sign)
 
-def convert_integer(buffer : int, base : int) -> list[int]:
+def convert_integer(buffer : int) -> list[int]:
+    base = BASE_INT
     i  : int = 0
     try:
-        int_magnitude = int(log(buffer, base)) #throws ValueError if buffer == 0
+        int_magnitude = int(log(buffer, base)) # throws ValueError if buffer == 0
         if int_magnitude == 0: 
             raise Exception()
-        digit_i : list[int] = [0 for n in range(int_magnitude + 1)] #list of enough zeros to handle integer part's magnitude
+        digit_i : list[int] = [0 for n in range(int_magnitude + 1)] # list of enough zeros to handle integer part's magnitude
         while buffer != 0 and i <= int_magnitude:
             my_remainder = buffer%base
             buffer = buffer//base
             digit_i[i] = my_remainder
             i += 1
-        return digit_i[::-1] #reverse list to proper orientation for binary representation AKA this algorithm gives results 'backwards'
-    except ValueError: #catches log(0)
+        if base > 10:
+            digit_i = convert_number_list(digit_i)
+        return digit_i[::-1] # reverse list to proper orientation for alt base representation
+                             # AKA this algorithm gives results 'backwards'
+    except ValueError: # catches log(0)
         return [0]
-    except: #catches log_b(a) where 0 < a < b
+    except: # catches log_b(a) where 0 < a < b
+        if base > 10:
+            return convert_number_list([buffer])
         return [buffer]
 
-def convert_fractional(buffer : float, base : int, precision : int) -> list[int]:
+def convert_fractional(buffer : float) -> list[int]:
+    base = BASE_INT
+    precision = MAX_DIGITS
     i : int = 0
-    digit_i : list[int] = [0 for n in range(precision)] #precision-length list of zeros
+    digit_i : list[int] = [0 for n in range(precision)] # precision-length list of zeros
     while buffer != 0 and i < precision:
         buffer *= base
         digit_i[i] = int(buffer)
@@ -79,4 +118,6 @@ def convert_fractional(buffer : float, base : int, precision : int) -> list[int]
     for elem in digit_i:
         if elem != 0:
             return cut_trailing_zeros(digit_i)
+    if base > 10:
+        digit_i = convert_number_list(digit_i)
     return []
--- MA_02/main.py
+++ MA_02/main.py
@@ -1,12 +1,40 @@
+from ipaddress import v4_int_to_packed
 import sys
 from dec_to_bin.d2b import *
 
+FLAG_LOOKUP = {'b': set_base_int, 'p': set_max_digits}
+
+def handle_flags(flag : str, *args) -> int:
+    offset : int = 1
+    for j in range(1, len(flag)):
+        settr = FLAG_LOOKUP[flag[j]] # raises keyerror if provided flag is not in FLAG_LOOKUP
+        try:
+            settr(args[1 + offset]) # raises IndexError if missing arguments
+            offset += 1
+        except IndexError:
+            raise ValueError
+    return offset
+
 def main(*args):
+    i : int = 0
+    if args[1][0] == '-': # if flag is detected
+        try:
+            i = handle_flags(args[1], *args)
+        except KeyError:
+            print('''TODO BAD FLAG ERROR MESSAGE''')
+            return
+        except ValueError:
+            print('''TODO MISSING ARGUMENTS ERROR''')
+            return
     results : str = results_header()
-    for arg in args[1:]:
-        dec_bin : DecBinTuple = dec_to_bin(float(arg))
-        results += result_string(*dec_bin)
+    for arg in args[i + 1:]:
+        dec_alt : DecAltTuple = dec_to_alt(float(arg))
+        results += result_string(*dec_alt)
     print(results)
 
 if __name__ == '__main__':
-    main(*sys.argv)
+    args = sys.argv
+    if len(args) == 1:
+        print('''TODO USAGE MESSAGE''')
+    else: 
+        main(*args)
